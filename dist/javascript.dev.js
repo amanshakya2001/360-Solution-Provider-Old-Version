e4e2f007358acbb63a761a308695dcb5ea398ee3
crazyboy1803 <amanshakya360@gmail.com>
2021-10-08 00:11:15 +0530

    final commit

--- dist/javascript.dev.js
+++ dist/javascript.dev.js
@@ -1,7 +1,23 @@
 "use strict";
 
+var a = 0;
+
 function myfunction() {
-  alert("kaam krr raha hai");
-  document.getElementById("logincard").position = "100px";
+  if (a % 2 == 0) {
+    document.getElementById("logincard").style.display = "block";
+    a++;
+  } else {
+    document.getElementById("profile").style.display = "none";
+    document.getElementById("login").innerHTML = "LOGIN";
+    a++;
+  }
+
+  event.preventDefault();
+}
+
+function myfunction1() {
+  document.getElementById("logincard").style.display = "none";
+  document.getElementById("profile").style.display = "block";
+  document.getElementById("login").innerHTML = "LOGOUT";
 }
 //# sourceMappingURL=javascript.dev.js.map
