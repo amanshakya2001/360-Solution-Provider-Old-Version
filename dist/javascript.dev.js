708bf63ea669e32ba1efff744dca14d5e40a27cc
crazyboy1803 <amanshakya360@gmail.com>
2021-10-07 23:34:23 +0530

    final commit

---
+++ dist/javascript.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function myfunction() {
+  alert("kaam krr raha hai");
+  document.getElementById("logincard").position = "100px";
+}
+//# sourceMappingURL=javascript.dev.js.map
---
+++ javascript.js
@@ -0,0 +1,4 @@
+function myfunction(){
+    alert("kaam krr raha hai");
+    document.getElementById("logincard").position = "100px";
+}
