f5936a461c735bfa012a9c513ae41f47d6f70e65
crazyboy1803 <amanshakya360@gmail.com>
2021-10-19 16:52:05 +0530

    final commit

--- javascript.js
+++ javascript.js
@@ -9,17 +9,34 @@ function myfunction(){
         document.getElementById("profile").style.display ="none";
         document.getElementById("login").innerHTML = "LOGIN";
         alert("Logout Successfully");
+        document.querySelector(".text").value = "";
+        document.querySelector(".password").value = "";
         document.getElementById('frame1').setAttribute("src","./home.html");
         a++;
     }
     event.preventDefault();
 }
 function myfunction1(){
-        document.getElementById("text").
-        document.getElementById("logincard").style.display = "none";
-        document.getElementById("profile").style.display ="block";
-        document.getElementById("login").innerHTML = "LOGOUT";
-        alert("Login Successfully");
+        if(document.querySelector(".text").value == "" && document.querySelector(".password").value == "")
+        {   
+            alert("Enter Username and Password");
+        }
+        else if(document.querySelector(".text").value == "")
+        {
+            alert("Enter Username");
+        }
+        else if(document.querySelector(".password").value == "")
+        {
+            alert("Enter Password")
+        }
+        else
+        {
+            document.getElementById("logincard").style.display = "none";
+            document.getElementById("profile").style.display ="block";
+            document.getElementById("login").innerHTML = "LOGOUT";
+            alert("Login Successfully");
+            document.querySelector(".text").value = document.querySelector(".username").value;
+        }
 }
 function sidebar(){
     document.getElementById('chk').checked = false;
